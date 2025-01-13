4a18e41b375deea6c7970e5de9ec33a2265e1e60
Frosty.py <gabrielpythonista@gmail.com>
2025-01-13 01:08:52 +0000

    Add files via upload

---
+++ folder_organizer.py
@@ -0,0 +1,9 @@
+#criar um programa que pegue uma pasta que esta com arquivos de diversos tipos e
+# coloque em pastas separadas de acordo com a extensão do arquivo
+
+import os
+import shutil
+
+downloads_folder = f'{__file__}'.replace('C:/projetos/Envio email/folder_organizer.py', 'C:/Users/Z064/Downloads')
+
+print(downloads_folder)
